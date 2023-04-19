964c0fde0340a6c664bd9a8b4bf47f141fa215d0
lucas-saggioro <xsaggioro@gmail.com>
2023-04-19 14:02:30 -0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalcMedia
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) //Calcular
+        {
+            float soma, media, valor;
+            soma = 0;
+
+            foreach (Control controle in this.Controls)
+            {
+                if (controle is TextBox)
+                {
+                    valor = Convert.ToSingle(((TextBox)controle).Text);
+                    soma += valor;
+                }
+
+                media = soma / 4;
+                this.Controls["Label7"].Text = media.ToString();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) //Limpar
+        {
+            foreach (Control controle in this.Controls)
+            {
+                if (controle is TextBox)
+                {
+                    ((TextBox)controle).Text = "";
+                }
+
+                this.Controls["Label7"].Text = "...";
+            }
+        }
+    }
+}
